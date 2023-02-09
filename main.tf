44b811d0d5dda1b946fd54a227692fb680deabc3
Airidas <airidassien64@gmail.com>
2023-02-09 14:48:14 +0000

    testing

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
-variable "region" {
-  type = string
+variable "AWS_Access_key_ID" {
+  description = "AWS access key ID."
+}
+
+variable "AWS_Secret_access_key" {
+  description = "AWS secret access key."
 }
 
 provider "aws" {
-  region = "aws_region"
-  access_key = "AWS_Access_key_ID"
-  secret_key = "AWS_Secret_access_key"
+  region = var.region
+  access_key = var.AWS_Access_key_ID
+  secret_key = var.AWS_Secret_access_key
 }
 
 # Create our VPC
