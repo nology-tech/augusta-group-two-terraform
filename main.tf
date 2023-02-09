39afda977b86f5ecc04aea62e2c2aca047bd024c
Adam Sackfield <adam@adamsackfield.uk>
2023-02-09 20:21:53 +0000

    minor tweaks to Game.js

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ most_recent = true
 
 filter {
     name   = "name"
-    values = ["augusta-baked-image-database-group-two-database*"]
+    values = ["augusta-baked-image-application-group-two-database*"]
   }
 
 filter {
@@ -66,14 +66,18 @@ module "db-tier" {
       to_port = 27017
       protocol = "tcp"
       cidr_blocks = "${module.application-tier.subnet_cidr_block}"
+    },
+     {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0" # TODO Currently open to all, will create an array for each memeber (add 32 to the end)
     }
   ]
 }
 
-
 data "aws_ami" "app-ami" {
   most_recent = true
-
 
 filter {
     name   = "name"
@@ -92,14 +96,14 @@ filter {
 }
 
 module "application-tier" {
-  name                    = "group-two-app"
   source                  = "./modules/application-tier"
   vpc_id                  = "${aws_vpc.group-two-application-deployment.id}"
   route_table_id          = "${aws_route_table.group-two-rt.id}"
   cidr_block              = "10.16.0.0/24"
-  user_data               = templatefile("./scripts/app_user_data.sh", { mongodb_ip=module.db-tier.private_ip })
+  user_data               = templatefile("./scripts/app_user_data.sh", { db_ip=module.db-tier.private_ip })
   ami_id                  = "${data.aws_ami.app-ami.id}" 
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true 
+  name                    = "Snake-Group-Two"
 
   ingress = [
     {
@@ -112,7 +116,7 @@ module "application-tier" {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = "0.0.0.0/0" # Currently open to all, will create an array for each memeber (add 32 to the end)
+      cidr_blocks = "0.0.0.0/0" # TODO Currently open to all, will create an array for each memeber (add 32 to the end)
     }
   ]
 }
